9f3f84a3424d94f201856d3305729b91017ca26b
uselesspart <uselesspartinfo@gmail.com>
2022-04-06 22:06:16 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title> Книга отзывов</title>
+        <style>
+        
+        </style>
+    </head>
+    <body>
+        <form method="post">
+            Имя <br><input type="text" name="first_name"><br>
+            Фамилия <br><input type="text" name="last_name"><br>
+            Номер телефона <br><input type="text" name="phone"><br>
+        </form>
+        <form action="a.php" method="post">
+            <br><input type="datetime-local" name="start_date"><br>
+            <br><input type="datetime-local" name="end_date"><br>
+        </form>
+    </body>
+</html>
